a157b59f496fde707ab15afe441b8ac78ee12927
jeremy Bächtold <jeremy.bachi10@gmail.com>
2026-01-07 15:24:28 +0000

    Try fixing this error: `Console Error: Invalid collection reference. Col

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -2,7 +2,7 @@
 
 import { initializeFirebase } from '@/firebase/index.server';
 import { initialServiceCategories, initialServices } from '@/lib/initial-data';
-import { collection, writeBatch } from 'firebase/firestore';
+import { collection, doc, writeBatch } from 'firebase/firestore';
 
 export async function populateDatabase() {
   try {
@@ -12,19 +12,15 @@ export async function populateDatabase() {
     // Populate serviceCategories
     const categoriesCollection = collection(firestore, 'serviceCategories');
     initialServiceCategories.forEach(category => {
-      const docRef = collection(categoriesCollection, category.id).parent;
-      if (docRef) {
-          batch.set(docRef, category);
-      }
+      const docRef = doc(categoriesCollection, category.id);
+      batch.set(docRef, category);
     });
 
     // Populate services
     const servicesCollection = collection(firestore, 'services');
     initialServices.forEach(service => {
-        const docRef = collection(servicesCollection, service.id).parent;
-        if(docRef) {
-            batch.set(docRef, service);
-        }
+        const docRef = doc(servicesCollection, service.id);
+        batch.set(docRef, service);
     });
 
     await batch.commit();
